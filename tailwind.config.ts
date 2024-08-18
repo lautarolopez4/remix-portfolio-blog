a83b10fe4a254c96757d72cd0e6bfa89ea38bf91
lautarolopez4 <lautarolopez4@protonmail.com>
2024-08-18 20:01:50 -0300

    Agregado contenido de prueba y configuraciṕn de los packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,14 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'xs': '0.75rem',
+        'sm-custom': '0.8125rem', // Tamaño intermedio entre xs y sm
+        'sm': '0.875rem',
+      },
+
+    },
   },
   plugins: [],
 } satisfies Config;
